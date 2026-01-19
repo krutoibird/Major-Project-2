81a46a6462ef4b5f4e6346eb3fa4833189f6c2e5
Ljy071210 <kimoji20071210@hotmail.com>
2026-01-18 22:10:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-
 import pygame
-from menu import main_menu, run_level, level_menu
-from game import Game
-from settings import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
+from menu import main_menu, level_menu
+from settings import SCREEN_WIDTH, SCREEN_HEIGHT
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
 
 state = "main_menu"
 
 while True:
-
     if state == "main_menu":
-        state = run_main_menu(screen, clock)
+        state = main_menu()
 
-    elif state == "level_select":
-        choice = run_level_select(screen, clock)
-        if choice == "main_menu":
-            state = "main_menu"
-        else:
-            # start chosen level
-            game = Game(level=choice)  # We can choose levels now
-            game.run()
-            state = "main_menu"
+    elif state == "level_menu":
+        state = level_menu()
 
     else:
-        pygame.quit()
         break
+
+pygame.quit()
+
