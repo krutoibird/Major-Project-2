f88705b76604bbd51beb9f1d7d2814fa4c63fa01
Ljy071210 <kimoji20071210@hotmail.com>
2026-01-18 22:13:01 -0500

    Create levels.py

---
+++ levels.py
@@ -0,0 +1,15 @@
+from level_1 import LEVEL_1_MAP
+from level_2 import LEVEL_2_MAP
+from level_3 import LEVEL_3_MAP
+from level_4 import LEVEL_4_MAP
+from level_5 import LEVEL_5_MAP
+from level_6 import LEVEL_6_MAP
+
+LEVELS = {
+    1: LEVEL_1_MAP,
+    2: LEVEL_2_MAP,
+    3: LEVEL_3_MAP,
+    4: LEVEL_4_MAP,
+    5: LEVEL_5_MAP,
+    6: LEVEL_6_MAP,
+}
