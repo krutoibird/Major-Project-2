ec09eb625fef08d0b7057f533f3a4795bca7c113
Ljy071210 <kimoji20071210@hotmail.com>
2026-01-18 22:11:53 -0500

    Update worm.py

--- worm.py
+++ worm.py
@@ -1,16 +1,16 @@
 from settings import TILE_SIZE, GRID_WIDTH, GRID_HEIGHT
 
 class Worm:
-    def __init__(self, game):
+    def __init__(self, game, start_pos):
         self.game = game
 
-        start_x = 3
-        start_y = 1
+        start_x, start_y = start_pos
 
+        
         self.segments = [
             (start_x, start_y),
             (start_x - 1, start_y),
-            (start_x - 2, start_y)
+            (start_x - 2, start_y),
         ]
 
         self.direction = (1, 0)
@@ -22,22 +22,22 @@ class Worm:
     def head(self):
         return self.segments[0]
 
-    def set_direction(self, direction):
-        self.direction = direction
-
     def try_move(self, direction):
-        self.set_direction(direction)
         dx, dy = direction
-
         hx, hy = self.head()
         new_head = (hx + dx, hy + dy)
 
+        
         if not (0 <= new_head[0] < GRID_WIDTH):
             return
         if not (0 <= new_head[1] < GRID_HEIGHT):
             return
-        if self.game.block.is_block(new_head):  
+
+        
+        if self.game.block.is_block(new_head):
             return
+
+        
         if new_head in self.segments:
             return
 
@@ -49,16 +49,11 @@ class Worm:
             return
         self.gravity_counter = 0
 
+        
         supported = False
-
-        for (sx, sy) in self.segments:
-            below = (sx, sy + 1)
-
-            if below[1] >= GRID_HEIGHT:
-                supported = True
-                break
-
-            if self.game.block.is_block(below):
+        for (x, y) in self.segments:
+            below = (x, y + 1)
+            if below[1] >= GRID_HEIGHT or self.game.block.is_block(below):
                 supported = True
                 break
 
@@ -81,6 +76,7 @@ class Worm:
         else:
             self.segments.pop()
 
+        
         for apple in self.game.apples:
             if apple.active and apple.position == new_head:
                 apple.active = False
@@ -91,9 +87,13 @@ class Worm:
 
     def draw(self):
         hx, hy = self.segments[0]
-        self.game.screen.blit(self.game.head_img, (hx * TILE_SIZE, hy * TILE_SIZE))
+        self.game.screen.blit(
+            self.game.head_img,
+            (hx * TILE_SIZE, hy * TILE_SIZE)
+        )
 
-        for gx, gy in self.segments[1:]:
-            px = gx * TILE_SIZE
-            py = gy * TILE_SIZE
-            self.game.screen.blit(self.game.body_img, (px, py))
+        for x, y in self.segments[1:]:
+            self.game.screen.blit(
+                self.game.body_img,
+                (x * TILE_SIZE, y * TILE_SIZE)
+            )
